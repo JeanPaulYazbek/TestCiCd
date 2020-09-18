dc12448246d77ba85964c6892a6ce692e2f23307
JeanPaulYazbek <jeanpaulyf98@hotmail.com>
2020-09-18 10:58:26 -0400

    Added CODECOV badge, and modified pom to add junit 5 and remove junit 3

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -1,18 +1,29 @@
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-
-public class ClassTest {
+public class ClassTest {	
 	
-
 	@Test
 	public void funTest() {
+		
+		Assert.assertEquals("Hello","Hello");
+	}
+	
+	@Test
+	public void funTest2() {
+		
+		Assert.assertEquals("Hello","Hello");
+	}
+	
+	@Test
+	public void funTest3() {
+		
+		Assert.assertEquals("Hello","Hello3");
+	}
+	
+	@Test
+	public void funTest4() {
 		
 		Assert.assertEquals("Hello","Hello");
 	}
