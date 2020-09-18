250bdd147ed8268484df505136531c02d6844e7e
JeanPaulYazbek <jeanpaulyf98@hotmail.com>
2020-09-17 21:00:04 -0400

    Added CODECOV badge, and modified pom to add junit 5 and remove junit 3

--- src/test/java/ClassTest.java
+++ src/test/java/ClassTest.java
@@ -1,8 +1,8 @@
 package com.cicd.cicdappliedtospringbootjavaapp;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.Assert;
+import org.junit.jupiter.Test;
+import org.junit.jupiter.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.test.context.junit4.SpringRunner;
